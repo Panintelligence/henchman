e806a4f8544d7b701c058579d94c8896c8f6f655
Tiago Correia <tiago.f.a.correia@gmail.com>
2019-01-11 10:58:37 +0000

    Fixed issue linking

--- src/services/jira.js
+++ src/services/jira.js
@@ -3,7 +3,14 @@ const http = require('https');
 
 
 const jiraGet = (url, success, fail) => {
-  http.get(url, (res) => {
+  const options = {
+    method: "GET",
+    headers: {
+      "Authorization": `Basic ${config.token}`,
+      "Content-Type": "application/json"
+    }
+  }
+  http.request(url, options, (res) => {
     const chunks = [];
     res.on('data', (chunk) => {
       chunks.push(chunk);
@@ -29,41 +36,42 @@ const jiraGet = (url, success, fail) => {
     if(fail){
       fail(e);
     }
-  });
+  })
+  .end();
 };
 
-const _matchesToIssueLinks = (message, matches, isPermitted, jiraConfig) => {
+const _matchesToIssueLinks = (message, matches, isPermitted, config) => {
   return matches.map(m => m.trim())
     .filter((m) => {
       return message.split(/\s/g).filter((word) => {
           return word.includes(m) && (word.includes('http') || word.includes('@') || (word.includes('<') && word.includes('>')))
         }).length === 0 &&
-        !message.includes(`${jiraConfig.protocol}://${jiraConfig.host}/browse/${m}`)
+        !message.includes(`${config.protocol}://${config.host}/browse/${m}`)
     })
     .filter((m) => {
-      return jiraConfig.projects.some((p) => m.includes(p.code)) || isPermitted
+      return config.projects.some((p) => m.includes(p.code)) || isPermitted
     })
     .filter((m) => {
-      return jiraConfig.projects.some((p) => {
+      return config.projects.some((p) => {
         return Number(m.includes(p.code) ? m.split(`${p.code}-`)[1] : m) >= p.issueStart
       });
     })
     .reduce((acc, issue)=>{
-      const defaultProject = jiraConfig.projects.find(p => p.default);
+      const defaultProject = config.projects.find(p => p.default);
       if(!acc.includes(issue) && !acc.includes(`${defaultProject.code}-${issue}`) && !acc.map(a=>`${defaultProject.code}-${a}`).includes(issue)){
         acc.push(issue);
       }
       return acc;
     }, [])
     .map((issueNumber) => {
-      const defaultProject = jiraConfig.projects.find(p => p.default);
-      if (isPermitted && !jiraConfig.projects.some(p => issueNumber.includes(p.code))) {
+      const defaultProject = config.projects.find(p => p.default);
+      if (isPermitted && !config.projects.some(p => issueNumber.includes(p.code))) {
         if (Number(issueNumber) >= defaultProject.issueStart) {
-          return `${jiraConfig.protocol}://${jiraConfig.host}/browse/${defaultProject.code}-${issueNumber}`
+          return `${config.protocol}://${config.host}/browse/${defaultProject.code}-${issueNumber}`
         }
         return null;
       }
-      return `${jiraConfig.protocol}://${jiraConfig.host}/browse/${issueNumber}`
+      return `${config.protocol}://${config.host}/browse/${issueNumber}`
     })
     .filter((m) => {
       return !!m
@@ -75,7 +83,8 @@ const matchesToIssueLinks = (message, matches, isPermitted) => {
 }
 
 const checkIssueExists = (link, success, fail) => {
-  jiraGet(link, success, (e) => {
+  const issueNr = link.split('/browse/')[1];
+  jiraGet(`${config.protocol}://${config.host}/rest/api/2/issue/${issueNr}`, success, (e) => {
     console.log(e);
     fail(e);
   })
