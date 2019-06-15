63fefd3754e6f29f00e61cbf2c39490d91c80f57
Tiago Correia <tiago.f.a.correia@gmail.com>
2019-06-15 18:42:02 +0100

    Correcting folder for the volume.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM node:11
 
 RUN mkdir -p /code/henchman-discord-bot
 COPY src /code/henchman-discord-bot/bot
-VOLUME /code/henchman-discord-bot/bot
 WORKDIR /code/henchman-discord-bot/bot
 RUN npm install
 ENTRYPOINT ["./entrypoint.sh"]
--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ PARENT=`ip route show | grep docker0 | awk '{print \$9}'`
 
 if [ ! -d "$CONFIG_DIR" ]; then
     sudo mkdir -p "$CONFIG_DIR"
+    sudo chmod 777 "$CONFIG_DIR"
+    sudo chown $USER "$CONFIG_DIR"
 fi
 
 if [[ "$1" == "rebuild" ]]; then
@@ -23,10 +25,12 @@ if [[ "$HAS_IMAGE" == "" ]]; then
 	docker build -t ${IMAGE} .
 fi
 
+cp -r ./src/* "$CONFIG_DIR/bot"
+
 docker run \
 	--restart unless-stopped \
 	--name "${NAME}" \
 	--add-host parent:${PARENT} \
-	-v ${CONFIG_DIR}:/code/discord-builder-bot \
+	-v ${CONFIG_DIR}:/code/henchman-builder-bot \
 	-tdi \
 	"${IMAGE}" "$2"
